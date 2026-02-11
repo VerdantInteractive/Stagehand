49962a0a2f395ed27a039c2e80a1ae870aac14dd
Alp <gokalp84@gmail.com>
2026-02-12 00:01:28 +1100

    Improve scripts\update_dependencies.sh

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
+set -e
 
-git fetch godot-cpp master
-git subtree pull --prefix dependencies/godot-cpp godot-cpp master --squash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "${SCRIPT_DIR}/.."
 
-git fetch flecs master
-git subtree pull --prefix dependencies/flecs flecs master --squash
+# Configurable target branches for each dependency (can be overridden via env)
+: ${GODOT_CPP_BRANCH:=master}
+: ${FLECS_BRANCH:=master}
+: ${GOOGLETEST_BRANCH:=v1.17.x}
 
-git fetch googletest v1.17.x
-git subtree pull --prefix dependencies/googletest googletest v1.17.x --squash
+
+git fetch godot-cpp "${GODOT_CPP_BRANCH}"
+git subtree pull --prefix dependencies/godot-cpp godot-cpp "${GODOT_CPP_BRANCH}" --squash
+
+git fetch flecs "${FLECS_BRANCH}"
+git subtree pull --prefix dependencies/flecs flecs "${FLECS_BRANCH}" --squash
+
+git fetch googletest "${GOOGLETEST_BRANCH}"
+git subtree pull --prefix dependencies/googletest googletest "${GOOGLETEST_BRANCH}" --squash
