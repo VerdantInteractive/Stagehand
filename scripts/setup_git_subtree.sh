2b981894c6282951cade9dca53a61dc1929d0e6d
Gökalp Özcan <gokalp@mail.com>
2026-02-21 14:13:38 +1100

    asdf

--- scripts/setup_git_subtree.sh
+++ scripts/setup_git_subtree.sh
@@ -4,7 +4,7 @@ git remote add -f godot-cpp https://github.com/godotengine/godot-cpp.git
 git subtree add --prefix dependencies/godot-cpp godot-cpp master --squash
 
 git remote add -f googletest https://github.com/google/googletest.git
-git subtree add --prefix dependencies/googletest googletest main --squash
+git subtree add --prefix dependencies/googletest googletest v1.17.x --squash
 
 git remote add -f flecs git@github.com:VerdantInteractive/flecs.git
 git subtree add --prefix dependencies/flecs flecs master --squash
