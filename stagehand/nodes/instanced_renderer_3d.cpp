328c892669df696f22282805424191b68de3d9bb
Alp <gokalp84@gmail.com>
2026-03-09 21:16:32 +1100

    Update config warnings when an individual LOD config changes

--- stagehand/nodes/instanced_renderer_3d.cpp
+++ stagehand/nodes/instanced_renderer_3d.cpp
@@ -186,7 +186,24 @@ void InstancedRenderer3D::set_prefabs_rendered(const godot::PackedStringArray &p
 }
 
 void InstancedRenderer3D::set_lod_levels(const godot::TypedArray<InstancedRenderer3DLODConfiguration> &p_lod_levels) {
+    const godot::Callable warnings_callable = callable_mp(static_cast<godot::Node *>(this), &godot::Node::update_configuration_warnings);
+
+    for (int i = 0; i < lod_levels.size(); ++i) {
+        godot::Ref<InstancedRenderer3DLODConfiguration> previous_lod_level = lod_levels[i];
+        if (previous_lod_level.is_valid() && previous_lod_level->is_connected("changed", warnings_callable)) {
+            previous_lod_level->disconnect("changed", warnings_callable);
+        }
+    }
+
     lod_levels = p_lod_levels;
+
+    for (int i = 0; i < lod_levels.size(); ++i) {
+        godot::Ref<InstancedRenderer3DLODConfiguration> lod_level = lod_levels[i];
+        if (lod_level.is_valid() && !lod_level->is_connected("changed", warnings_callable)) {
+            lod_level->connect("changed", warnings_callable);
+        }
+    }
+
     update_configuration_warnings();
 }
 
@@ -275,6 +292,36 @@ void InstancedRenderer3D::_bind_methods() {
                                  godot::PropertyInfo(godot::Variant::PACKED_STRING_ARRAY, "discovered_instance_uniforms", godot::PROPERTY_HINT_NONE, "",
                                                      godot::PROPERTY_USAGE_EDITOR | godot::PROPERTY_USAGE_READ_ONLY),
                                  "set_discovered_instance_uniforms", "get_discovered_instance_uniforms");
+}
+
+void InstancedRenderer3DLODConfiguration::set_mesh(const godot::Ref<godot::Mesh> &p_mesh) {
+    mesh = p_mesh;
+    emit_changed();
+}
+
+void InstancedRenderer3DLODConfiguration::set_visibility_range_begin(float p_value) {
+    visibility_range_begin = p_value;
+    emit_changed();
+}
+
+void InstancedRenderer3DLODConfiguration::set_visibility_range_end(float p_value) {
+    visibility_range_end = p_value;
+    emit_changed();
+}
+
+void InstancedRenderer3DLODConfiguration::set_visibility_range_begin_margin(float p_value) {
+    visibility_range_begin_margin = p_value;
+    emit_changed();
+}
+
+void InstancedRenderer3DLODConfiguration::set_visibility_range_end_margin(float p_value) {
+    visibility_range_end_margin = p_value;
+    emit_changed();
+}
+
+void InstancedRenderer3DLODConfiguration::set_visibility_range_fade_mode(godot::RenderingServer::VisibilityRangeFadeMode p_value) {
+    visibility_range_fade_mode = p_value;
+    emit_changed();
 }
 
 void InstancedRenderer3DLODConfiguration::_bind_methods() {
--- stagehand/nodes/instanced_renderer_3d.h
+++ stagehand/nodes/instanced_renderer_3d.h
@@ -16,35 +16,35 @@ class InstancedRenderer3DLODConfiguration : public godot::Resource {
     GDCLASS(InstancedRenderer3DLODConfiguration, godot::Resource)
 
   public:
-    void set_mesh(const godot::Ref<godot::Mesh> &p_mesh) { mesh = p_mesh; }
+    void set_mesh(const godot::Ref<godot::Mesh> &p_mesh);
     [[nodiscard]] godot::Ref<godot::Mesh> get_mesh() const { return mesh; }
 
     // Starting distance from which the GeometryInstance3D will be visible, taking visibility_range_begin_margin into account as well. The default value of 0 is
     // used to disable the range check.
-    void set_visibility_range_begin(float p_value) { visibility_range_begin = p_value; }
+    void set_visibility_range_begin(float p_value);
     [[nodiscard]] float get_visibility_range_begin() const { return visibility_range_begin; }
 
     // Distance from which the GeometryInstance3D will be hidden, taking visibility_range_end_margin into account as well. The default value of 0 is used to
     // disable the range check.
-    void set_visibility_range_end(float p_value) { visibility_range_end = p_value; }
+    void set_visibility_range_end(float p_value);
     [[nodiscard]] float get_visibility_range_end() const { return visibility_range_end; }
 
     // Margin for the visibility_range_begin threshold. The GeometryInstance3D will only change its visibility state when it goes over or under the
     // visibility_range_begin threshold by this amount. If visibility_range_fade_mode is VISIBILITY_RANGE_FADE_DISABLED, this acts as a hysteresis distance. If
     // visibility_range_fade_mode is VISIBILITY_RANGE_FADE_SELF or VISIBILITY_RANGE_FADE_DEPENDENCIES, this acts as a fade transition distance and must be set
     // to a value greater than 0.0 for the effect to be noticeable.
-    void set_visibility_range_begin_margin(float p_value) { visibility_range_begin_margin = p_value; }
+    void set_visibility_range_begin_margin(float p_value);
     [[nodiscard]] float get_visibility_range_begin_margin() const { return visibility_range_begin_margin; }
 
     // Margin for the visibility_range_end threshold. The GeometryInstance3D will only change its visibility state when it goes over or under the
     // visibility_range_end threshold by this amount. If visibility_range_fade_mode is VISIBILITY_RANGE_FADE_DISABLED, this acts as a hysteresis distance. If
     // visibility_range_fade_mode is VISIBILITY_RANGE_FADE_SELF or VISIBILITY_RANGE_FADE_DEPENDENCIES, this acts as a fade transition distance and must be set
     // to a value greater than 0.0 for the effect to be noticeable.
-    void set_visibility_range_end_margin(float p_value) { visibility_range_end_margin = p_value; }
+    void set_visibility_range_end_margin(float p_value);
     [[nodiscard]] float get_visibility_range_end_margin() const { return visibility_range_end_margin; }
 
     // Controls which instances will be faded when approaching the limits of the visibility range.
-    void set_visibility_range_fade_mode(godot::RenderingServer::VisibilityRangeFadeMode p_value) { visibility_range_fade_mode = p_value; }
+    void set_visibility_range_fade_mode(godot::RenderingServer::VisibilityRangeFadeMode p_value);
     [[nodiscard]] godot::RenderingServer::VisibilityRangeFadeMode get_visibility_range_fade_mode() const { return visibility_range_fade_mode; }
 
   protected:
